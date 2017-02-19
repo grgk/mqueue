cdace06d64d8ccff3063e296f38cfa36d27f3301
Grzegorz Karpiak <gfx@karpiak.pl>
2017-02-19 20:07:42 +0100

    removed parent

--- src/Exception/InvalidMessageException.php
+++ src/Exception/InvalidMessageException.php
@@ -8,6 +8,5 @@ class InvalidMessageException extends Exception
 {
     public function __construct($message, $code = 0, Exception $previous = null)
     {
-        parent::__construct($message, $code, $previous);
     }
 }
